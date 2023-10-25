90f224f0a24e4172a4d2ce89039570531c2bc1e7
Ichiro Maruta <ichiro.maruta@gmail.com>
2023-10-25 20:48:54 +0900

    add save button

--- test/test.m
+++ test/test.m
@@ -1,7 +1,7 @@
 sp = serialport("COM7",115200);
 
 for ph=0:0.02:100*pi
-    line = sprintf("%e\t%e\t%e",sin(ph),ph*10*sin(ph+2*pi/3),100*sin(ph+4*pi/3));
+    line = sprintf("%e\t%e\t%e",ph,sin(ph),ph*10*sin(ph+2*pi/3),100*sin(ph+4*pi/3));
     sp.writeline(line)
     pause(0.01)
 end
